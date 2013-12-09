be0526185a6e917e19b1157739993bafe9c7afe2
giraffine <giraffine@gmail.com>
2013-12-09 14:22:05 +0800

    Refactor for Mask
    Refactor for Mask

--- Dimmer/src/giraffine/dimmer/DimmerService.java
+++ Dimmer/src/giraffine/dimmer/DimmerService.java
@@ -37,12 +37,9 @@ public class DimmerService extends Service{
 	public static final int DEFAULTLEVEL = 1000;
 	public static int lastLevel = DEFAULTLEVEL;
 	
-	WindowManager mWindowManager;
-	WindowManager.LayoutParams mWindowParams;
-	int maskLength = 0;
-	View mMaskView;
-	boolean mActing = false;
-	Notification mNotification;
+	private boolean mActing = false;
+	private Notification mNotification;
+	private Mask mMask = null;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -82,7 +79,7 @@ public class DimmerService extends Service{
 		lastLevel = (int)(((float)currentBrightness)/255*500 + 500);
 		sendBroadcast(new Intent(Dimmer.REFRESH_INDEX));
 
-		initMask();
+		mMask = new Mask(this);
 		
         ContentResolver resolver = getContentResolver();
         resolver.registerContentObserver(Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS), true, new ContentObserver(null){
@@ -105,21 +102,6 @@ public class DimmerService extends Service{
         		return;
         	}
         });
-/*        
-        SensorManager mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Log.e(Dimmer.TAG, "Sensor: getMaximumRange=" + mPressure.getMaximumRange() + ", getMinDelay=" + mPressure.getMinDelay());
-        mSensorManager.registerListener(new SensorEventListener(){
-			@Override
-			public void onAccuracyChanged(Sensor arg0, int arg1) {
-				Log.e(Dimmer.TAG, "onAccuracyChanged: " + arg0.toString() + ", arg1=" + arg1);
-			}
-			@Override
-			public void onSensorChanged(SensorEvent arg0) {
-				Log.e(Dimmer.TAG, "onSensorChanged: " + arg0.values[0]);
-			}
-        }, mPressure, SensorManager.SENSOR_DELAY_FASTEST);
-*/        
     }
 	
 	@Override
@@ -149,79 +131,13 @@ public class DimmerService extends Service{
 //		Log.e(Dimmer.TAG, "onStartCommand(): " + lastLevel);
 		return lastLevel>500 ? START_NOT_STICKY : START_STICKY;
 	}
-	
-	private void initMask()
+	private void adjustLevel(int i, boolean finished)
 	{
-		mWindowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-		mWindowParams = new WindowManager.LayoutParams();
-
-		mWindowParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
-		mWindowParams.flags |= 
-			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE 
-			| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS 
-			| WindowManager.LayoutParams.FLAG_FULLSCREEN
-			| WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-			| WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-			| WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-			| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-		mWindowParams.gravity = Gravity.LEFT | Gravity.TOP;
-		Point p = new Point();
-		mWindowManager.getDefaultDisplay().getSize(p);
-		mWindowParams.x = 0;
-		mWindowParams.y = 0;
-		maskLength = ( p.x > p.y ? p.x : p.y) +300;
-		mWindowParams.width = maskLength;
-		mWindowParams.height = maskLength;
-		mWindowParams.format = 1;
-		mWindowParams.alpha = (float)0;	// default is transparent
-
-		LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mMaskView = inflater.inflate(R.layout.mask_window, null);
-//		mMaskView.setAlpha((float)1);	// == android:alpha
-		
-//		adjustLevel(lastLevel);
-		mWindowManager.addView(mMaskView, mWindowParams);
-	}
-	public void adjustLevel(int i, boolean finished)
-	{
-		Log.e(Dimmer.TAG, "adjustLevel: " + i);
 		if(i > 500)
-		{
-			float index = ((float)(i - 500))/500;
-			if(finished)
-				BrightnessUtil.setBrightness((int)(index*255));
-			else
-				maskBrightness(index);
 			removeNotification();
-		}
 		else
-		{
-			if(finished)
-				BrightnessUtil.setBrightness(0);
-			i = 500 - i;
-			adjustMask(((float)i/500));
 			postNotification();
-		}
-	}
-	public void maskBrightness(float value)
-	{
-		Log.e(Dimmer.TAG, "maskBrightness: " + value);
-		if(value <= 0.01)	return;	// for padfone: value ~0.0 would fully dark screen 
-		mWindowParams.width = 1;	// reduce memory usage
-		mWindowParams.height = 1;	// reduce memory usage
-		mWindowParams.screenBrightness = value;
-		mWindowManager.updateViewLayout(mMaskView, mWindowParams);
-	}
-	public void adjustMask(float alpha)
-	{
-		Log.e(Dimmer.TAG, "adjustMask: " + alpha);
-		if(alpha > 0.98) alpha = (float)0.9;
-		else if(alpha < 0) alpha = (float)0;
-//		mMaskView.setAlpha(alpha);	// control parent window is much safe
-		mWindowParams.width = maskLength;
-		mWindowParams.height = maskLength;
-		mWindowParams.alpha = alpha; 
-		mWindowManager.updateViewLayout(mMaskView, mWindowParams);
+		mMask.adjustLevel(i, finished);
 	}
 	public void resetLevel()
 	{
@@ -229,14 +145,7 @@ public class DimmerService extends Service{
 		removeNotification();
 		stopSelf();
 		Process.killProcess(Process.myPid());
-/*
-		int currentBrightness = BrightnessUtil.getBrightness(); 
-		lastLevel = (int)(((float)currentBrightness)/255*500 + 500);
-		Log.e(Dimmer.TAG, "resetLevel() lastLevel: " + lastLevel);
-		BrightnessUtil.setPreferLevel(lastLevel);
-		adjustMask(0);
-		onStartCommand(null, 0 , 0);
-*/	}
+	}
 	Handler mHandler = new Handler(){
 		public void handleMessage(Message msg) {
 			 switch (msg.what) {
---
+++ Dimmer/src/giraffine/dimmer/Mask.java
@@ -0,0 +1,93 @@
+package giraffine.dimmer;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Mask {
+	
+	private static Context mContext;
+	private WindowManager mWindowManager;
+	private WindowManager.LayoutParams mWindowParams;
+	private int maskLength = 0;
+	private View mMaskView;
+
+	public Mask(Context context){
+		mContext = context;
+		initMask();
+	}
+	private void initMask()
+	{
+		mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+		mWindowParams = new WindowManager.LayoutParams();
+
+		mWindowParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+		mWindowParams.flags |= 
+			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE 
+			| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS 
+			| WindowManager.LayoutParams.FLAG_FULLSCREEN
+			| WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+			| WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+			| WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+			| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+		mWindowParams.gravity = Gravity.LEFT | Gravity.TOP;
+		Point p = new Point();
+		mWindowManager.getDefaultDisplay().getSize(p);
+		mWindowParams.x = 0;
+		mWindowParams.y = 0;
+		maskLength = ( p.x > p.y ? p.x : p.y) +300;
+		mWindowParams.width = maskLength;
+		mWindowParams.height = maskLength;
+		mWindowParams.format = 1;
+		mWindowParams.alpha = (float)0;	// default is transparent
+
+		LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mMaskView = inflater.inflate(R.layout.mask_window, null);
+//		mMaskView.setAlpha((float)1);	// == android:alpha
+		
+		mWindowManager.addView(mMaskView, mWindowParams);
+	}
+	public void adjustLevel(int i, boolean finished)
+	{
+		Log.e(Dimmer.TAG, "adjustLevel: " + i);
+		if(i > 500)
+		{
+			float index = ((float)(i - 500))/500;
+			if(finished)
+				BrightnessUtil.setBrightness((int)(index*255));
+			else
+				maskBrightness(index);
+		}
+		else
+		{
+			if(finished)
+				BrightnessUtil.setBrightness(0);
+			i = 500 - i;
+			adjustMask(((float)i/500));
+		}
+	}
+	public void maskBrightness(float value)
+	{
+		Log.e(Dimmer.TAG, "maskBrightness: " + value);
+		if(value <= 0.01)	return;	// for padfone: value ~0.0 would fully dark screen
+		mWindowParams.width = 1;	// reduce memory usage
+		mWindowParams.height = 1;	// reduce memory usage
+		mWindowParams.screenBrightness = value;
+		mWindowManager.updateViewLayout(mMaskView, mWindowParams);
+	}
+	public void adjustMask(float alpha)
+	{
+		Log.e(Dimmer.TAG, "adjustMask: " + alpha);
+		if(alpha > 0.98) alpha = (float)0.9;
+		else if(alpha < 0) alpha = (float)0;
+//		mMaskView.setAlpha(alpha);	// control parent window is much safe
+		mWindowParams.width = maskLength;
+		mWindowParams.height = maskLength;
+		mWindowParams.alpha = alpha; 
+		mWindowManager.updateViewLayout(mMaskView, mWindowParams);
+	}
+}
